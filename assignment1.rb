f99610676250b4da7eff458eb51c804720060f89
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-06-03 18:56:30 -0400

    first submission, need to finish key value fetch on first assignemnt.

---
+++ assignment1.rb
@@ -0,0 +1,15 @@
+Time to examine the data:
+
+What are the keys in this (top-level) hash? HINT: use the keys method.
+house.keys
+house["objects"]
+house["meta"]
+What is the data type of the value that goes with the first key?
+Array
+What is the data type of the value that goes with the last key?
+Hash
+How many representatives are there in this collection?
+house["objects"].length
+20
+Get the URL for the photo of one representative into a variable.
+array.map { |hash| hash['name'] }
